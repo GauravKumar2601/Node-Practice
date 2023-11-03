9d88dd4e2071e42dab8ee4e91b3f61ea89df654b
GauravKumar260100 <gaurav.kumar.mcs21@cs.du.ac.in>
2023-11-03 11:58:48 +0530

    Second commit

--- Native Modules/index.js
+++ Native Modules/index.js
@@ -1,7 +1,14 @@
 //Using a Native node module
 const fs = require("fs");
 
+//Creating and writing to a new file
 fs.writeFile("message.txt", "Hello from native modules", (err) => {
   if (err) throw err;
   else console.log("Data written to file");
+});
+
+//Reading from the file
+fs.readFile("./message.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  else console.log(data);
 });
